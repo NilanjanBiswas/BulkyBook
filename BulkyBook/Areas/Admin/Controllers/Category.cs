fcfad6e51ca0d85636de0221b0c1e91b286d73a3
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2022-04-23 11:47:44 +0600

    category Controller API+View index +Js+Ajax

---
+++ BulkyBook/Areas/Admin/Controllers/Category.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class Category : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork; 
+        public Category(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GellALL();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
